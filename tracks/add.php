0a769ce914fa65b401bf503e88fb1bebad78d507
elemermelada <iiasanmi9@gmail.com>
2021-11-29 19:43:55 +0100

    ADD SONGS VIA YOUTUBE FEATURE ADDED
    Click in "PIRACIFY" to add new songs via youtube url
    
    * Input url in the box
    * youtube-dl is used to retrieve song (thus needed in server-side)
    * Song is automatically added with recommended format

---
+++ tracks/add.php
@@ -0,0 +1,36 @@
+
+<link rel="stylesheet" href="../main.css">
+<link rel="shortcut icon" type="image/png" href="../favicon.png">
+<title>Add new songs!</title>
+
+<body>
+
+<center>
+<h1>Add new songs!</h1>
+<div class="element">
+
+<?php
+
+$url=$_POST['url'];
+
+if ($url=="") {
+
+echo '
+<form action="" method="POST">
+	<input type="text" name="url" placeholder="YouTube URL"><br>
+	<input type="submit">
+</form>';	
+	
+} else {
+	
+	$com = 'youtube-dl ' . $url . ' -f 140 --output "%(id)s.mp3"';
+	$out = shell_exec($com); 
+	echo "Command: " . $com . '</div><div class="element">' . "Output: " . $out;
+	
+	
+}
+
+?>
+
+</div>
+</center>
