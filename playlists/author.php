aa474b478cbaa692bf2ea79ecfb6163a4259d720
elemermelada <iiasanmi9@gmail.com>
2022-07-11 20:00:21 +0200

    author #2

--- playlists/author.php
+++ playlists/author.php
@@ -2,9 +2,16 @@
 
     $author = $_GET['author'];
     $tracks=glob("../tracks/*.mp3");
-
+    
     for ($i=1;$i<count($tracks);$i++) {
-        echo substr($tracks[$i],10) . "<br>";
+        
+        $trackname = substr($tracks[$i],10);
+        $trackjson = file_get_contents("../tracks/metadata/" . $trackname);
+
+        if ($trackjson!==false) {
+
+        }
+
     }
 
 ?>
--- playlists/author_search.php
+++ playlists/author_search.php
@@ -1,8 +1,19 @@
 <?php
 
-    $author = $_GET['author'];
+    $authors = Array("Unknown artist");
     $tracks=glob("../tracks/*.mp3");
 
-    print_r($tracks);
+    for ($i=1;$i<count($tracks);$i++) {
+        
+        $trackname = substr($tracks[$i],10);
+        $trackjson = file_get_contents("../tracks/metadata/" . $trackname);
+
+        if ($trackjson!==false) {
+            $trackdata = json_decode($trackjson);
+            print_r($trackdata);
+            echo "<p>";
+        }
+
+    }
 
 ?>
