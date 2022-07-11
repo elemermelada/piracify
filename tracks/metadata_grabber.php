da10abf130119cfccd5e886a5e8338741adee3ff
elemermelada <iiasanmi9@gmail.com>
2022-07-11 21:09:11 +0200

    Update metadata_grabber.php

--- tracks/metadata_grabber.php
+++ tracks/metadata_grabber.php
@@ -3,6 +3,8 @@
     $json = file_get_contents('php://input');
     $tracks = json_decode($json, true)['tracks'];
 
+    $tracks_metadata = Array();
+
     if ($tracks=="") {
         die('<form action="" method="POST"><input name="tracks"><input type="submit"></form>');
     }
@@ -10,8 +12,12 @@
     for ($i=1;$i<count($tracks);$i++) {
 
         $trackjson = file_get_contents("metadata/" . $tracks[$i] . ".json");
-        echo $trackjson;
+        $trackdata = json_decode($trackjson, true);
+
+        $tracks_metadata[$trackdata["track"]] = $trackdata;
 
     }
+
+    echo json_encode($tracks_metadata);
 
 ?>
