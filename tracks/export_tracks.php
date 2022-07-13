7d3dd318e7652952eb6f650c9d9ef09b5fa04fb1
elemermelada <iiasanmi9@gmail.com>
2022-07-13 16:15:18 +0200

    Update export_tracks.php

--- tracks/export_tracks.php
+++ tracks/export_tracks.php
@@ -3,8 +3,19 @@
     $tracks = glob("*.mp3");
 
     foreach ($tracks as $track) {
-        $trackjson = file_get_contents("metadata/" . substr($track,0,strlen($track)-4) . ".json");
-        echo $trackjson . "<br>";
+        $trackname = substr($track,0,strlen($track)-4);
+        $trackjson = file_get_contents("metadata/" . $trackname . ".json");
+
+        if ($trackjson!="") {
+            $trackdata = json_decode($trackjson,true);
+            $trackname = $trackdata["author"] . " - " . $trackdata["title"];
+        }
+
+
+        copy($track, "../exported/" . $trackname . ".mp3")
+
+        echo $track . " exported as " . "../exported/" . $trackname . ".mp3" . "<br>";
+
     }
 
 ?>
