7ae38701bff87389f9078fa52a77e0b54ee71a92
Elemer San Miguel <iiasanmi9@gmail.com>
2022-12-20 10:46:20 +0100

    Update export_tracks.php

--- tracks/export_tracks.php
+++ tracks/export_tracks.php
@@ -1,18 +1,31 @@
 <?php
 
-    $tracks = glob("*.mp3");
+$com = 'zip exported/output.zip *.mp3';
+$out = shell_exec($com);
+echo $out;
+echo '<p>'
 
-    foreach ($tracks as $track) {
-        $trackname = substr($track,0,strlen($track)-4);
-        $trackjson = file_get_contents("metadata/" . $trackname . ".json");
 
-        if ($trackjson!="") {
-            $trackdata = json_decode($trackjson,true);
-            $trackname = $trackdata["author"] . " - " . $trackdata["title"];
-        }
+$tracks = glob("*.mp3");
 
-        copy($track, "exported/" . $trackname . ".mp3");
-        echo $track . " exported as " . "exported/" . $trackname . ".mp3" . "<br>";
+foreach ($tracks as $track) {
+    $trackname = substr($track,0,strlen($track)-4);
+    $trackjson = file_get_contents("metadata/" . $trackname . ".json");
+
+    if ($trackjson!="") {
+        $trackdata = json_decode($trackjson,true);
+        $trackname = $trackdata["author"] . " - " . $trackdata["title"];
     }
+
+    $com = 'zip exported/output.zip *.mp3';
+    $out = shell_exec($com);
+    echo $out;
+    echo '<p>'
+
+    $com = 'printf "@ ' . $track . '\n@=' . $trackname . '\n" | zipnote -w exported/output.zip'
+    $out = shell_exec($com);
+    echo $out;
+    echo '<p>'
+}
 
 ?>
